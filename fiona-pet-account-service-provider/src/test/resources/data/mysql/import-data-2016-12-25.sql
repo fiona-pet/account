b7fef8bce46e48bfa9944db48f9a88e036c6a9b1
tom <tom@daoshenggroup.com>
2017-01-16 09:28:35 +0800

    add menu

---
+++ fiona-pet-account-service-provider/src/test/resources/data/mysql/import-data-2016-12-25.sql
@@ -0,0 +1,5 @@
+-- 处方模版管理 --
+insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0608','nurse');
+insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0608','doctor');
+insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0608','face');
+insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0608','sales-staff');
---
+++ fiona-pet-account-service-provider/src/test/resources/data/mysql/import-data-2017-01-15-19-44.sql
@@ -0,0 +1,10 @@
+-- ----------------------------
+-- Table data for t_role_menu
+-- ----------------------------
+-- 结算单查询 --
+insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0316','face');
+insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0316','sales-staff');
+
+-- 宠物类型管理 --
+insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0610','face');
+insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0610','sales-staff');
