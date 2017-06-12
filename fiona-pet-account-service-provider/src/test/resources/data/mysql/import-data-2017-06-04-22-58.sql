458bdfe8538812091e215092f56e408350eb2e0a
tom <tom@daoshenggroup.com>
2017-06-12 09:09:42 +0800

    权限分配

--- fiona-pet-account-service-provider/src/test/resources/data/mysql/import-data-2017-06-04-22-58.sql
+++ fiona-pet-account-service-provider/src/test/resources/data/mysql/import-data-2017-06-04-22-58.sql
@@ -7,3 +7,5 @@ insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0313','sales-s
 insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0313','nurse');
 insert into t_role_menu (id,menu_code,role_code) values(uuid(),'FM0313','doctor');
 
+
+insert into t_user_role (user_id,role_id) values('402881ce5b85710c015b98c1ad7a0003','59061794-7df9-49b5-a5e4-b786073c2b80');
